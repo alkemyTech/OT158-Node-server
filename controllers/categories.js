699af8c5493072f05bb43b9edbce7dae6db142e2
JuanSilva27 <juan.silva.0270@gmail.com>
2022-03-25 17:55:34 -0300

    arreglo en el controlador de Categories

--- controllers/categories.js
+++ controllers/categories.js
@@ -1,23 +1,25 @@
 const categoriesService = require('../services/categories');
 
-const getAll = async (req, res, next) => {
-    try {
-        const result = await categoriesService.getAll(req);
-        res.status(200).json({
-            meta: {
-                status: 200,
-                total: result.length,
-                url: '/api/movie/:id'
-            },
-            data: result
-        });
+module.exports = {
+    getAll: async (req, res, next) => {
+        try {
+            const result = await categoriesService.getAll(req);
+            res.status(200).json({
+                meta: {
+                    status: 200,
+                    total: result.length,
+                    url: ''
+                },
+                data: result
+            });
+        }
+        catch (error) {
+            return res.status(400).json({
+                status: 400,
+                message: error
+            })
+        }
     }
-    catch (error) {
-        return res.status(400).json({
-            status: 400,
-            message: error
-        })
-    }
-};
+}
 
-module.exports = { getAll };
+module.exports = {};
