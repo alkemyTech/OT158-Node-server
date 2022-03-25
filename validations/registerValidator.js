a34fd7170fe4f4c917af4b378913f8bce27f74cd
Edgar-Rios <sistunla2020@gmail.com>
2022-03-25 17:04:52 -0300

    added validation to create user

---
+++ validations/registerValidator.js
@@ -0,0 +1,26 @@
+const { check, body } = require('express-validator');
+const db = require('../models');
+// const db = require('../models');
+
+module.exports = [
+    check('firstName')
+    .notEmpty()
+    .withMessage('name required'),
+
+    check('lastName')
+    .notEmpty()
+    .withMessage('last name required'),
+
+    check('email')
+    .isEmail()
+    .withMessage('email format invalid'),
+
+    body('email').custom( value => {
+        return db.Users.findOne({
+            where: { email: value }
+        })
+        .then(user => {
+            if(user) return Promise.reject("email is already in use");
+        })
+    }),
+]
