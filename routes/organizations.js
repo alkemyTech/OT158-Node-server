f876f6f842351b1207a9405f12f35402a182af86
giornn0 <shaaamsuu@gmail.com>
2022-03-25 20:12:54 -0300

    removed dates on create/update (not necesary),fixed typo in routes, removed routes from app.js

--- routes/organizations.js
+++ routes/organizations.js
@@ -7,7 +7,7 @@ router.get('/', organizationsController.getAll);
 /* POST a new organization. */
 router.post('/', organizationsController.create);
 /* PUT an edit on a organization. */
-router.put('/;id', organizationsController.update);
+router.put('/:id', organizationsController.update);
 /* DELETE an organization(soft delete). */
 router.delete('/:id', organizationsController.remove);
 
--- services/organizations.js
+++ services/organizations.js
@@ -4,15 +4,12 @@ const getAll = async (req)=>{
   return result
 }
 const create = async ({body})=>{
-  body.createdAt= new Date();
-  body.updatedAt= new Date();
   const result = await organizationsRepository.create(body);
   return result
 }
 const update = async ({body,params})=>{
   const {id} = params;
   const options = {where:{id}}
-  body.updatedAt= new Date();
   const result = await organizationsRepository.update(body,options);
   return result
 }
