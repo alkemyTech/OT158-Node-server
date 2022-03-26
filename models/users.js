ca5f3e30ea4657d0b89e486b89eae25a27e2daa5
JuanSilva27 <juan.silva.0270@gmail.com>
2022-03-25 23:12:03 -0300

    se arregla error de tipeado en modelo Users

--- models/users.js
+++ models/users.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      User.belongsTo(models.Roles, {as: 'role'});
+      Users.belongsTo(models.Roles, {as: 'role'});
     }
   };
   Users.init({
