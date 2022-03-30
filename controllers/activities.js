0cf03d498c2681640a79f538f35ba577d949793f
Ayelen85 <ayelenrotta@gmail.com>
2022-03-30 19:57:07 -0300

    modify activities controllers

--- controllers/activities.js
+++ controllers/activities.js
@@ -1,89 +1,14 @@
-const express = require('express');
-const activitiesService = require('../services/activities');
-
-/*const createActivities = (req, res) => {
-    console.log(req.body);
-    res.send('received');
-}*/
-
-
-
-const getAll = async (req, res, next) => {
-    try {
-        const result = await activitiesService.getAll(req);
-        res.status(200).json({
-            data: result
-        });
-    }
-    catch (e) {
-        next(e);
-    }
-};
-
+const activitiesService = require('../services/activities')
 const create = async (req, res, next) => {
     try {
-        let activities = req.body;
-        activities = await activitiesService.save(activities);
-        res.status(200).json({
-            data: result
-        });
+      const result = await activitiesService.create(req);
+      res.status(200).json({ data: result });
+    } catch (error) {
+      next(error);
     }
-    catch (e) {
-        next(e);
-    }
+  };
+
+  module.exports = {
+    create
 };
-
-const update = async (req, res, next) => {
-    try {
-        const { id } = req.params;
-        let activities = req.body;
-
-        const activitiesUpdated = await activitiesService.update(id, activities);
-        res.status(200).json({
-            data: result
-        });
-    }
-    catch (e) {
-        next(e);
-    }
-};
-
-const getById = async (req, res) => {
-    try {
-        const activities = await activitiesService.findById(req.params.id);
-        res.status(200).json({
-            data: result
-        });
-    }
-    catch (e) {
-        next(e);
-    }
-};
-
-const remove = async (req, res, next) => {
-    try {
-        const { id } = req.params;
-        const activities = await activitiesService.remove(id);
-        res.status(200).json({
-            data: result
-        });
-    }
-    catch (e) {
-        next(e);
-    }
-};
-
-module.exports = {
-    getAll,
-    create,
-    update,
-    getById,
-    remove
-}
-
-
-
-
-
-
 
