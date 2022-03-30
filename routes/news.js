65fccc37b54beb5ed60fe32d9332dfb9d3be3a9c
giornn0 <shaaamsuu@gmail.com>
2022-03-30 20:10:15 -0300

    deleted news model and migrations

--- routes/news.js
+++ routes/news.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const newsController = require('../controllers/news');
 const { validData } = require('../middlewares/news');
-const { adminValidator } = require('../middlewares/users');
+const { adminValidator } = require('../middlewares/adminValidator');
 
 router.post('/',adminValidator,validData,newsController.create)
 
