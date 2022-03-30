67fceb338264ea41b4fbf61fc1b33d647ac0cadd
Edgar-Rios <sistunla2020@gmail.com>
2022-03-30 12:57:24 -0300

    add association in model Slides

--- models/slides.js
+++ models/slides.js
@@ -11,6 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      Slides.belongsTo(models.Organizations, {as: 'organization'});
     }
   }
   Slides.init({
