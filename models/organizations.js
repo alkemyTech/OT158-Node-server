603d7eec124e7023731c738f585cb442e8d7b380
Rigol Muhammad Ali <79057078+giornn0@users.noreply.github.com>
2022-03-31 08:14:45 -0300

    Create mmc organization (#10)
    * Remove and ignore package-lock.json
    
    * Repository pattern example
    
    * creation of controller, model, migration, services, repo and routes for Organization
    
    * changed organization controller response to {data: result}
    
    * changed entity Organization to Organizations, and files to be plurals too
    
    * fixed plurals in organizations model, added created,updated at values in create/update organication service
    
    * removed dates on create/update (not necesary),fixed typo in routes, removed routes from app.js
    
    * deestructure model in repository
    
    * added getById functionality to Organization entity
    
    * fixed softDeletes and timestamps
    
    * moved timestamps and paranoid options where they belong in organization model
    
    * removed functionality of services, repository for Organizations
    
    * fixed services params
    
    * removed service, repository and routes for Organizations
    
    * removed method in controller
    
    * added deletedAt
    
    * added deletedAt
    
    * fixed nullables values
    
    Co-authored-by: manuelfrancisco <franciscomanueljose@gmail.com>

---
+++ models/organizations.js
@@ -0,0 +1,45 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Organizations extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Organizations.init(
+    {
+      name: { 
+        type: DataTypes.STRING,
+        allowNull: false 
+      },
+      image: { 
+        type: DataTypes.STRING,
+        allowNull: false 
+      },
+      address: DataTypes.STRING,
+      phone: DataTypes.INTEGER,
+      email: { 
+        type: DataTypes.STRING,
+        allowNull: false 
+      },
+      deletedAt: DataTypes.DATE,
+      welcomeText: { 
+        type: DataTypes.TEXT,
+        allowNull: false 
+      },
+      aboutUsText: DataTypes.TEXT,
+    },
+    {
+      sequelize,
+      timestamps: true,
+      paranoid: true,
+      modelName: "Organizations",
+    }
+  );
+  return Organizations;
+};
