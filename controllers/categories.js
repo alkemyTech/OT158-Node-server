69b81a3a0ff8fe2328a490ef88d73fd4e6c351e1
JuanSilva27 <juan.silva.0270@gmail.com>
2022-03-31 17:41:02 -0300

    se modifica la manera de exportar funciones

--- controllers/categories.js
+++ controllers/categories.js
@@ -1,23 +1,23 @@
 const categoriesService = require('../services/categories');
 
-module.exports = {
-    getAll: async (req, res, next) => {
-        try {
-            const result = await categoriesService.getAll(req);
-            res.status(200).json({
-                meta: {
-                    status: 200,
-                    total: result.length,
-                    url: ''
-                },
-                data: result
-            });
-        }
-        catch (error) {
-            return res.status(400).json({
-                status: 400,
-                message: error
-            })
-        }
+const getAll = async (req, res, next) => {
+    try {
+        const result = await categoriesService.getAll(req);
+        res.status(200).json({
+            meta: {
+                status: 200,
+                total: result.length,
+                url: ''
+            },
+            data: result
+        });
+    }
+    catch (error) {
+        return res.status(400).json({
+            status: 400,
+            message: error
+        })
     }
 }
+
+module.exports = { getAll }
