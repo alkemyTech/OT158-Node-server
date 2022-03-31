632b0680f9029de07056b963a26b1f5cb5669fd7
Ayelen85 <ayelenrotta@gmail.com>
2022-03-31 13:49:00 -0300

    delete activities in app.js

--- app.js
+++ app.js
@@ -25,12 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/activities', activitiesRouter)
 
-//endpoint activities
-app.get('/activities', (req,res) => {
-res.send({ message: 'I am in activities'})
-})
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
