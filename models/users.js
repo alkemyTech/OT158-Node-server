2a4d573be21ea4890a386c4999584144d6737140
Martin L. Ruiz <alacran.mlr@gmail.com>
2022-03-31 18:18:06 -0300

    fix conflicts model users

--- models/users.js
+++ models/users.js
@@ -1,31 +1,48 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class Users extends Model {
-    /**
+	class Users extends Model {
+		/**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
-
-      User.belongsTo(models.Roles, {as: 'role'});
-
-    }
-  };
-  Users.init({
-    firstName: DataTypes.STRING,
-    lastName: DataTypes.STRING,
-    email: DataTypes.STRING,
-    image: DataTypes.STRING,
-    password: DataTypes.STRING,
-    roleId: DataTypes.INTEGER,
-    deletedAt: DataTypes.DATE
-  }, {
-    sequelize,
-    modelName: 'Users',
-  });
-  return Users;
+		static associate(models) {
+			Users.belongsTo(models.Roles, { as: 'role' });
+		}
+	}
+	Users.init(
+		{
+			firstName: {
+				type: DataTypes.STRING,
+				allowNull: false
+			},
+			lastName: {
+				type: DataTypes.STRING,
+				allowNull: false
+			},
+			email: {
+				type: DataTypes.STRING,
+				allowNull: false,
+				unique: true
+			},
+			password: {
+				type: DataTypes.STRING,
+				allowNull: false
+			},
+			photo: {
+				type: DataTypes.STRING,
+				allowNull: true
+			},
+			roleId: DataTypes.INTEGER,
+			deletedAt: DataTypes.DATE
+		},
+		{
+			sequelize,
+			modelName: 'Users',
+			timestamps: true,
+			paranoid: true
+		}
+	);
+	return Users;
 };
