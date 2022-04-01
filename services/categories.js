365ac931abec0e3d3864e0af6de6c7f36b66f5b2
JuanSilva27 <87153937+JuanSilva27@users.noreply.github.com>
2022-04-01 17:22:29 -0300

    Ot158 44 endpoint update de Categories (#40)
    * se crea endpoint update de Categories
    
    * se completa logica del service update
    
    * se completa logica del service update
    
    * se completa manejo de errores del endpoint update

--- services/categories.js
+++ services/categories.js
@@ -9,7 +9,27 @@ const getAll = async () => {
 };
 
 const create = async (req) => {
-  const result = await categoriesRepository.create(req)
+  const result = await categoriesRepository.create(req);
   return result;
 };
-module.exports = { getAll, create };
+
+const update = async (id, data) => {
+  const categories = await categoriesRepository.getById(id);
+  if (categories) {
+    const updatedCategorie =
+      await categoriesRepository.update(id, data);
+    if (updatedCategorie === 1) {
+      const result = await categoriesRepository.getById(id);
+      return result;
+    } else {
+      const error = new Error('Categories not updated');
+    error.status = 400;
+    throw error;
+    }
+  } else {
+    const error = new Error('Categories not found');
+    error.status = 404;
+    throw error;
+  }
+};
+module.exports = { getAll, create, update };
