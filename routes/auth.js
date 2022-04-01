708c3cdcd5d6818fd3969014a54a1a2c1521251f
Matias Minchaca <matiasminchaca1@gmail.com>
2022-04-01 13:20:30 -0300

    Merge branch 'dev' into 25-endpoint-authentication

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,8 @@
 const express = require('express');
-const { login } = require('../controllers/auth');
 const router = express.Router();
 const { loginValidation } = require('../middlewares/auth')
 
 /* POST users listing */
-router.post('/login', loginValidation, login )
+router.post('/login', loginValidation)
 
 module.exports = router;
