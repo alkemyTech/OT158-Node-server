bf60e95eb7a2beede220951c9e3d12bb5f963390
JuanSilva27 <juan.silva.0270@gmail.com>
2022-04-01 16:29:51 -0300

    se completa logica del service update

--- services/categories.js
+++ services/categories.js
@@ -9,26 +9,23 @@ const getAll = async () => {
 };
 
 const create = async (req) => {
+  const result = await categoriesRepository.create(req);
+  return result;
+};
 
-    const result = await categoriesRepository.create(req)
-    return result;
-}
-
-
-const update = async (id, data)=>{
-  const categories = await categoriesRepository.getById(id)
-  if(categories){
-    const updatedCategorie =await categoriesRepository.update(id,data)
-    if(updatedCategorie===1){
-      const result= await categoriesRepository.getById(id)
-      return result
+const update = async (id, data) => {
+  const categories = await categoriesRepository.getById(id);
+  if (categories) {
+    const updatedCategorie =
+      await categoriesRepository.update(id, data);
+    if (updatedCategorie === 1) {
+      const result = await categoriesRepository.getById(id);
+      return result;
+    } else {
+      return error;
     }
-    else{
-      return error
-    }
+  } else {
+    return error;
   }
-  else{
-    return error
-  }
-}
-module.exports = { getAll, create, update }
+};
+module.exports = { getAll, create, update };
