dcf68c72c6d53417194e0d9cbca7a7bf4b60622c
Matias Minchaca <85573304+MatiasMinchaca@users.noreply.github.com>
2022-04-01 12:30:36 -0300

    Ot158 73 endpoint to list members (#34)
    * Endpoint to list members
    
    * merge to dev

--- routes/members.js
+++ routes/members.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { create } = require('../controllers/members');
+const { create, getAll } = require('../controllers/members');
 const router = express.Router();
 const { creationValidation } = require('../middlewares/members')
 
+router.get('/', getAll);
 router.post('/', creationValidation, create);
 
 module.exports = router;
