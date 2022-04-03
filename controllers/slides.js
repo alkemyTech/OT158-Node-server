084685edd3595a2cc136aa73a90e0907f7730a6e
Edgar-Rios <sistunla2020@gmail.com>
2022-04-03 15:55:11 -0300

    changed method to export

--- controllers/slides.js
+++ controllers/slides.js
@@ -1,16 +1,16 @@
 const service = require('../services/slides');
 
+const create = async (req, res, next) => {
+    try {
+        let data = await service.create(req);
+        res.status(200).json({
+            data,
+        });
+    } catch (error) {
+        next(error);
+    }
+}
+
 module.exports = {
-
-    create: async (req, res, next) => {
-        try {
-            let data = await service.create(req);
-            res.status(200).json({
-                data,
-            });
-        } catch (error) {
-            next(error);
-        }
-    },
-
+    create,
 }
