1a09f9660dc4813fe9bba9a8476ea39d32943998
Martin L. Ruiz <alacran.mlr@gmail.com>
2022-04-03 19:53:19 -0300

    FIx admin roleID

--- middlewares/adminValidator.js
+++ middlewares/adminValidator.js
@@ -1,10 +1,12 @@
 const adminValidator = async (req, res, next) => {
-  const user = req.user;
+  const { roleId } = req.data;
   try {
-    if (user.role === "Admin") {
+    if (roleId === '1') {
       next();
     } else {
-      return res.status(403).json({ error: "Admin role required" });
+      return res
+        .status(403)
+        .json({ error: 'Admin role required' });
     }
   } catch (err) {
     return res.status(500).send({ error: err.message });
