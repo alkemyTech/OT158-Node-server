448fd30a1bf537eb703277b59547b5c0c203c230
Mario Del Campo <mdcgithubprojects@gmail.com>
2022-04-04 23:02:35 -0400

    minor changes

--- app.js
+++ app.js
@@ -3,20 +3,20 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const cors = require('cors')
-require('dotenv').config()
+const cors = require('cors');
+require('dotenv').config();
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const slidesRouter = require('./routes/slides');
-const categoriesRouter = require("./routes/categories")
+const categoriesRouter = require('./routes/categories');
 const contactRouter = require('./routes/contacts');
 const membersRouter = require('./routes/members');
-const testimonialsRoutes = require('./routes/testimonials')
+const testimonialsRoutes = require('./routes/testimonials');
 const authRouter = require('./routes/auth');
 
 const app = express();
-app.use(cors())
+app.use(cors());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -24,35 +24,37 @@ app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({
-  extended: false
-}));
+app.use(
+	express.urlencoded({
+		extended: false
+	})
+);
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/slides', slidesRouter);
-app.use("/categories", categoriesRouter)
-app.use("/contacts", contactRouter);
-app.use("/members", membersRouter);
-app.use('/testimonials', testimonialsRoutes)
-app.use("/auth", authRouter);
+app.use('/categories', categoriesRouter);
+app.use('/contacts', contactRouter);
+app.use('/members', membersRouter);
+app.use('/testimonials', testimonialsRoutes);
+app.use('/auth', authRouter);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
+app.use(function(req, res, next) {
+	next(createError(404));
 });
 
 // error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function(err, req, res, next) {
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+	// render the error page
+	res.status(err.status || 500);
+	res.render('error');
 });
 
 module.exports = app;
