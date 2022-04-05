9fcd9bf9434670d895b12646c74c62b7cff30b07
Mario Del Campo <mdcgithubprojects@gmail.com>
2022-04-04 22:52:46 -0400

    Adding a validation middleware of tesimonials input

---
+++ middlewares/testimonials.js
@@ -0,0 +1,19 @@
+const { check, validationResult } = require('express-validator');
+
+const testimonialsValidation = async (req, res, next) => {
+	await check('name', 'Name is required').exists().notEmpty().run(req);
+	await check('content', 'Content is required').exists().notEmpty().isEmail().run(req);
+
+	const result = validationResult(req);
+
+	if (!result.isEmpty()) {
+		return res.status(400).json({
+			ok: false,
+			errors: result.mapped()
+		});
+	}
+
+	next();
+};
+
+module.exports = { testimonialsValidation };
