a010495b5f979851ccf9313c1e9974dc3c303f9f
Mario Del Campo <mdcgithubprojects@gmail.com>
2022-04-05 20:46:35 -0400

    Service file for category by id + minor formating changes

--- services/categories.js
+++ services/categories.js
@@ -1,35 +1,47 @@
 const categoriesRepository = require('../repositories/categories');
 
 const getAll = async () => {
-  const allCategories = await categoriesRepository.getAll();
-  const result = allCategories.map((categorie) => {
-    return categorie.name;
-  });
-  return result;
+	const allCategories = await categoriesRepository.getAll();
+	const result = allCategories.map((categorie) => {
+		return categorie.name;
+	});
+	return result;
 };
 
 const create = async (req) => {
-  const result = await categoriesRepository.create(req);
-  return result;
+	const result = await categoriesRepository.create(req);
+	return result;
 };
 
 const update = async (id, data) => {
-  const categories = await categoriesRepository.getById(id);
-  if (categories) {
-    const updatedCategorie =
-      await categoriesRepository.update(id, data);
-    if (updatedCategorie === 1) {
-      const result = await categoriesRepository.getById(id);
-      return result;
-    } else {
-      const error = new Error('Categories not updated');
-    error.status = 400;
-    throw error;
-    }
-  } else {
-    const error = new Error('Categories not found');
-    error.status = 404;
-    throw error;
-  }
+	const categories = await categoriesRepository.getById(id);
+	if (categories) {
+		const updatedCategorie = await categoriesRepository.update(id, data);
+		if (updatedCategorie === 1) {
+			const result = await categoriesRepository.getById(id);
+			return result;
+		} else {
+			const error = new Error('Categories not updated');
+			error.status = 400;
+			throw error;
+		}
+	} else {
+		const error = new Error('Categories not found');
+		error.status = 404;
+		throw error;
+	}
 };
-module.exports = { getAll, create, update };
+
+const getById = async (id) => {
+	const category = await categoriesRepository.getById(id);
+
+	if (!category) {
+		const error = new Error('Category not found');
+		error.status = 404;
+		throw error;
+	}
+
+	return category;
+};
+
+module.exports = { getAll, create, update, getById };
