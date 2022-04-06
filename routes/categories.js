56cabb6d6e1a34b2284d3ca1a4f94ffe21bbb3ab
Mario Del Campo <mdcgithubprojects@gmail.com>
2022-04-05 22:25:52 -0400

    Adding the admin auth middleware

--- routes/categories.js
+++ routes/categories.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const { create, getAll, update, getById }= require("../controllers/categories");
 const { creationValidation }=require("../middleware/categories")
+const { adminValidator } = require('../middlewares/adminValidator');
 const router = express.Router();
 
 /* GET users listing. */
 router.get('/',getAll);
 router.post('/',creationValidation,create);
 router.put("/:id", update)
-router.get('/:id', getById)
+router.get('/:id', adminValidator, getById)
 
 module.exports = router;
