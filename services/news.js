7d5aeb91ef2d775aec4729b994af0ada947f405b
Mario Del Campo <mdcgithubprojects@gmail.com>
2022-04-05 21:44:43 -0400

    Adding the remove News service function

--- services/news.js
+++ services/news.js
@@ -5,6 +5,16 @@ const create =async({body})=>{
   const result = await newsRepository.create(body)
   return result
 }
+
+const remove = async id => {
+  const newToRemove = await newsRepository.getById(id);
+  if (!newToRemove) throw new Error('Bad request!');
+
+  const result = await newsRepository.remove(id);
+  return result
+};
+
 module.exports={
-  create
+  create,
+  remove
 }
