815778fba04633a6db0052f24f1dbeedc9c00ffe
Martin L. Ruiz <alacran.mlr@gmail.com>
2022-04-08 15:55:03 -0300

    espacios entre variables para que sea mas legible el codigo

--- services/users.js
+++ services/users.js
@@ -2,15 +2,13 @@ const usersRepository = require('../repositories/users');
 const bcrypt = require('bcryptjs');
 const { validationResult } = require('express-validator');
 const res = require('express/lib/response');
+const { NotFound, BadRequest } = require('../utils/status');
 
-
-const getAll = async (req) => {
-  const result = await usersRepository.getAll();
-  return result;
-}
+const getAll = () => {
+  return usersRepository.getAll();
+};
 
 const create = async (req) => {
-
   validationResult(req).throw();
 
   let user = { ...req.body };
@@ -18,22 +16,27 @@ const create = async (req) => {
 
   const result = await usersRepository.create(user);
   return result;
-}
+};
 
 const update = async (id, data) => {
-  const users = await usersRepository.getById(id);
-  if (users) {
-    const updatedUser = await usersRepository.update(id, data);
+  const user = await usersRepository.getById(id);
+
+  if (user) {
+    const updatedUser = await usersRepository.update( id, data);
+
     if (updatedUser) {
-      return updatedUser
+
+      return updatedUser;
+
     } else {
       const error = new Error('User not updated');
-      error.status = 400;
+      error.status = BadRequest;
       throw error;
     }
+
   } else {
     const error = new Error('User not found');
-    error.status = 404;
+    error.status = NotFound;
     throw error;
   }
 };
