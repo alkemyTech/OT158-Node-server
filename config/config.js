434fc01042a954fa37de6f44b22b0bacb75625bc
Matias Minchaca <matiasminchaca1@gmail.com>
2022-04-08 12:10:42 -0300

    add middleware ownership

--- config/config.js
+++ config/config.js
@@ -1,29 +1,32 @@
 require('dotenv').config()
 
 module.exports = {
-    "development": {
-        "username": process.env.DB_USER,
-        "password": process.env.DB_PASSWORD,
-        "database":  process.env.DB_NAME,
-        "host": process.env.DB_HOST,
-        "port": process.env.DB_PORT,
-        "dialect": "mysql",
-        "secretToken": process.env.SECRET_TOKEN
-    },
-    "test": {
-        "username": process.env.DB_USER,
-        "password": process.env.DB_PASSWORD,
-        "database": process.env.DB_NAME,
-        "host": process.env.DB_HOST,
-        "dialect": "mysql",
-        "secretToken": process.env.SECRET_TOKEN
-    },
-    "production": {
-        "username": process.env.DB_USER,
-        "password": process.env.DB_PASSWORD,
-        "database": process.env.DB_NAME,
-        "host": process.env.DB_HOST,
-        "dialect": "mysql",
-        "secretToken": process.env.SECRET_TOKEN
-    }
+  "development": {
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
+    "port": process.env.DB_PORT,
+    "dialect": "mysql",
+    "secretToken": process.env.SECRET_TOKEN,
+    "roleAdmin": process.env.ROLE_ADMIN
+  },
+  "test": {
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
+    "dialect": "mysql",
+    "secretToken": process.env.SECRET_TOKEN,
+    "roleAdmin": process.env.ROLE_ADMIN
+  },
+  "production": {
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
+    "dialect": "mysql",
+    "secretToken": process.env.SECRET_TOKEN,
+    "roleAdmin": process.env.ROLE_ADMIN
+  }
 }
--- middlewares/ownership.js
+++ middlewares/ownership.js
@@ -1,37 +1,41 @@
 const { verifyToken } = require('../modules/auth');
-const { roleId } = require('../config/config')
+const { roleAdmin } = require('../config/config').development;
 
 const ownershipValidator = (req, res, next) => {
-  const token = req.headers['Authorization'];
+  const authorization = req.get('authorization');
+
+  const token = authorization?.substring(7);
 
   const { id } = req.params;
 
-  if (!token) {
+  if (!authorization) {
     return res.status(401).json({
       auth: false,
       message: 'No token provided'
-    })
-  }
+    });
+  };
 
   try {
     const decoded = verifyToken(token);
-    req.userId = decoded.userId
-    req.roleId = decoded.roleId
 
-    if (decoded.roleId == roleId) {
-      return next()
-    }
+    req.userId = decoded.userId.toString();
 
-    if (decoded.userId != id) {
+    req.roleId = decoded.roleId?.toString();
+
+    if (req.roleId && req.roleId === roleAdmin?.toString()) {
+      return next();
+    };
+
+    if (req.userId !== id.toString()) {
       return res.status(403).json({
         auth: false,
         message: 'Invalid token'
-      })
-    }
+      });
+    };
   } catch (err) {
-    next(err)
-  }
-  next()
-}
+    next(err);
+  };
+  next();
+};
 
 module.exports = { ownershipValidator };
