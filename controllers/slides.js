0881e3b69c26ee44c88e057310ec44118890a0af
Edgar-Rios <sistunla2020@gmail.com>
2022-04-08 17:34:26 -0300

    restructuring the way to export methods

--- controllers/slides.js
+++ controllers/slides.js
@@ -1,16 +1,19 @@
 const service = require('../services/slides');
 
+/* methods */
+const create = async (req, res, next) => {
+
+  try {
+    let data = await service.create(req);
+    res.status(200).json({
+      data,
+    });
+  } catch (error) {
+    next(error);
+  }
+
+};
+
 module.exports = {
-
-    create: async (req, res, next) => {
-        try {
-            let data = await service.create(req);
-            res.status(200).json({
-                data,
-            });
-        } catch (error) {
-            next(error);
-        }
-    },
-
+  create,
 }
