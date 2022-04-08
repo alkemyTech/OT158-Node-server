9af62f3514a546f202165201e3214cce057a9a38
Edgar-Rios <sistunla2020@gmail.com>
2022-04-08 17:46:22 -0300

    added method in service slides

--- services/slides.js
+++ services/slides.js
@@ -1,18 +1,28 @@
 const slideRepository = require('../repositories/slides');
 
-const create = async (req)  => {
+const create = async (req) => {
 
-    let imageUrl = req.body.imageUrl;
+  let imageUrl = req.body.imageUrl;
 
-    /* 
-        Envio al servidor s3...
-    */
+  /* 
+      Envio al servidor s3...
+  */
 
-    const slide = { ...req.body };
-    slide.imageUrl = imageUrl;//cambiar por el link que retorne aws luego de la subida del archivos
+  const slide = {
+    ...req.body
+  };
+  slide.imageUrl = imageUrl; //cambiar por el link que retorne aws luego de la subida del archivos
 
-    const result = await slideRepository.create(slide);
-    return result;
+  const result = await slideRepository.create(slide);
+  return result;
 }
 
-module.exports = { create };
+const getAll = async (req) => {
+  const result = await slideRepository.getAll();
+  return result;
+}
+
+module.exports = {
+  create,
+  getAll
+};
