909786d6a8339690e5d93fc8056f4cf51404640f
JuanSilva27 <juan.silva.0270@gmail.com>
2022-04-09 10:05:26 -0300

    se agregan sugerencias para ordenar el codigo

--- repositories/users.js
+++ repositories/users.js
@@ -1,28 +1,20 @@
-const {
-  Users
-} = require('../models')
+const { Users } = require('../models');
 
 const getAll = async () => {
   return await Users.findAll();
-}
+};
 
 const create = async (user) => {
   let result = await Users.create(user);
   return result;
-}
-
-const remove = async (UserId) =>{
-  return await Users.destroy({ where:{ id : UserId } })
-}
-
-const getById = async(id)=>{
-  const result = await Users.findByPk(id)
-  return result
-}
-
-module.exports = {
-  getAll,
-  getById,
-  create,
-  remove
 };
+
+const remove = async (userId) => {
+  return await Users.destroy({ where: { id: userId } });
+};
+
+const getById = async (id) => {
+  return await Users.findByPk(id);
+};
+
+module.exports = { getAll, getById, create, remove };
