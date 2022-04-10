08bb65b61f1df8be65336bdd8d4f7265182e6fc5
Martin L. Ruiz <alacran.mlr@gmail.com>
2022-04-10 17:15:14 -0300

    validacion con !

--- services/organizations.js
+++ services/organizations.js
@@ -1,20 +1,18 @@
 const organizationRepository = require('../repositories/organizations');
-const { NotFound } = require("../utils/status");
+const { NotFound } = require('../utils/status');
 
 const getDataOrganization = async () => {
-  try {
-    const organization = await organizationRepository.getOne();
-    if (organization) {
-      return organization
-    } else {
-      const error = new Error('Bad Request');
-      error.status = NotFound;
-      throw error
-    };
-  } catch (error) {
-    return error;
-  }
-}
+  const organization = await organizationRepository.getOne();
 
+  if (!organization) {
+
+    const error = new Error('Organization not found');
+    error.status = NotFound;
+    throw error;
+
+  } else {
+    return await organization;
+  }
+};
 
 module.exports = { getDataOrganization };
