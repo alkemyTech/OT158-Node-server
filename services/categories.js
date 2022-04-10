deed6c2046b54cbafa43f5f8f6210ee0bbee6561
Matias Minchaca <matiasminchaca1@gmail.com>
2022-04-10 15:43:31 -0300

    fixing category service

--- services/categories.js
+++ services/categories.js
@@ -2,36 +2,36 @@ const categoriesRepository = require('../repositories/categories');
 const { throwError } = require('../utils/errorHandler');
 const { BadRequest, NotFound } = require('../utils/status');
 
+const UPDATED_STATE_APPROVED = 1;
+
 const getAll = async () => {
   const allCategories = await categoriesRepository.getAll();
-
   const result = allCategories.map((categorie) => {
     return categorie.name;
   });
-
   return result;
 };
 
 const create = async (req) => {
-  return await categoriesRepository.create(req);
+  const result = await categoriesRepository.create(req);
+  return result;
 };
 
 const update = async (id, data) => {
   const categories = await categoriesRepository.getById(id);
-
   if (categories) {
-    const updatedCategorie = await categoriesRepository.update(id, data);
 
-    if (updatedCategorie === 1) {
+    const updatedCategorieState = await categoriesRepository.update(id, data);
+
+    if (updatedCategorieState[0] === UPDATED_STATE_APPROVED) {
       return await categoriesRepository.getById(id);
     } else {
-      throwError('Categories not updated', BadRequest);
+      throwError('Category not updated', BadRequest);
     }
   } else {
     throwError('Category not found', NotFound);
   }
 };
-
 const removeCategory = async (id) => {
   const category = await categoriesRepository.getById(id);
 
