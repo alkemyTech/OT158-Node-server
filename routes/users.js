e9bb2a9c8a95cfe0d704d5a1106e850345c787c6
Rigol Muhammad Ali <79057078+giornn0@users.noreply.github.com>
2022-04-11 09:46:26 -0300

    OT158-36 endpoint-users-list (#52)
    * creation of functions flow
    
    * changed to simple return, added more specific name to list variable
    
    * fixed status response
    
    * fixed status response

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const usersController = require('../controllers/users');
+const { adminValidator } = require('../middlewares/adminValidator');
 const router = express.Router();
 
 /* GET users listing. */
+router.get('/', adminValidator, usersController.getAll);
 router.get('/', usersController.getAll);
 router.delete("/:id", usersController.removeUser)
 
