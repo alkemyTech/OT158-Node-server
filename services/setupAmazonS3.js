6a1fca7e88a52d9c3e08cb299f46d585ded1fcb6
Martin L. Ruiz <alacran.mlr@gmail.com>
2022-04-11 19:47:38 -0300

    intalacion de dependencia, keys de aws y servicio

---
+++ services/setupAmazonS3.js
@@ -0,0 +1,17 @@
+const S3 = require('aws-sdk/clients/s3');
+
+const region = process.env.AWS_REGION;
+const accessKeyId = process.env.AWS_ACCESS_KEY;
+const secretAccessKey = process.env.AWS_SECRET_KEY;
+const bucketName = process.env.AWS_BUCKET_NAME;
+
+const storage = new S3({
+    region,
+    accessKeyId,
+    secretAccessKey,
+    bucketName
+});
+
+
+
+module.exports = { storage };
