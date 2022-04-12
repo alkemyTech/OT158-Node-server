1f8185f48ff00c7d069bf70ce6efd987215c2623
Mario Del Campo <mdcgithubprojects@gmail.com>
2022-04-11 22:26:51 -0400

    Return directo + manejo de errores

--- controllers/testimonials.js
+++ controllers/testimonials.js
@@ -1,11 +1,12 @@
 const testimonialsService = require('../services/testimonials');
+const { BadRequest, OK } = require('../utils/status');
 
 const create = async (req, res, next) => {
 	try {
 		const data = await testimonialsService.create(req.body);
-		res.status(200).json(data);
+		res.status(OK).json(data);
 	} catch (error) {
-		next(error);
+		res.status(BadRequest).json(data);
 	}
 };
 
