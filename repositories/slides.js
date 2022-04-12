383728939f582526617535ce61af627afad05b1b
JuanSilva27 <juan.silva.0270@gmail.com>
2022-04-12 10:57:01 -0300

    se mergea dev y se resuelven conflictos

--- repositories/slides.js
+++ repositories/slides.js
@@ -2,7 +2,7 @@ const { Slides } = require('../models');
 
 const create = async (slide) => {
   return await Slides.create(slide);
-
+};
 const remove = async (userId) => {
   return await Slides.destroy({ where: { id: userId } });
 };
