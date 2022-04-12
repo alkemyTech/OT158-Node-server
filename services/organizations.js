5289d1cd3b9f0cb548af9541bfae01beab573d51
Martin L. Ruiz <alacran.mlr@gmail.com>
2022-04-12 19:56:17 -0300

    llamada a funcion directamente

--- services/organizations.js
+++ services/organizations.js
@@ -1,8 +1,8 @@
-const organizationRepository = require('../repositories/organizations');
+const { getOne } = require('../repositories/organizations');
 const { NotFound } = require('../utils/status');
 
 const getDataOrganization = async () => {
-  const organization = await organizationRepository.getOne();
+  const organization = await getOne();
 
   if (!organization) {
     const error = new Error('Organization not found');
