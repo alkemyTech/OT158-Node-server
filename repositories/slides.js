bfc87d56b97c55ac2f8710d33223c34296cb8aa4
Martin L. Ruiz <alacran.mlr@gmail.com>
2022-04-13 20:04:35 -0300

    try catch en services y update de slides

--- repositories/slides.js
+++ repositories/slides.js
@@ -6,11 +6,11 @@ const create = async (slide) => {
 };
 
 const getById = async (slideId) => {
-  return await Slides.findOne(slideId);
+  return await Slides.findByPk(slideId);
 };
 
 const update = async (id, body) => {
-  return await Slides.update(id, body);
+  return await Slides.update(body, {where: {id:id}});
 }
 
 module.exports = { create, getById, update };
--- services/slides.js
+++ services/slides.js
@@ -1,49 +1,54 @@
 const slideRepository = require('../repositories/slides');
-const { NotFound, Forbidden } = require("../utils/status")
+const { NotFound, Forbidden } = require('../utils/status');
 const { throwError } = require('../utils/errorHandler');
 
-const create = async (req)  => {
+const create = async (req) => {
+  let imageUrl = req.body.imageUrl;
 
-    let imageUrl = req.body.imageUrl;
-
-    /*
+  /*
         Envio al servidor s3...
     */
 
-    const slide = { ...req.body };
-    slide.imageUrl = imageUrl;//cambiar por el link que retorne aws luego de la subida del archivos
+  const slide = { ...req.body };
+  slide.imageUrl = imageUrl; //cambiar por el link que retorne aws luego de la subida del archivos
 
-    const result = await slideRepository.create(slide);
-    return result;
-}
+  const result = await slideRepository.create(slide);
+  return result;
+};
 
 const getById = async (slideId) => {
   const slide = await slideRepository.getById(slideId);
 
-  if(!slide){
-    const error = new Error("Slide not founde");
-    error.status = NotFound
-    throw error
+  if (!slide) {
+    const error = new Error('Slide not founde');
+    error.status = NotFound;
+    throw error;
   }
 
   return slide;
-}
+};
 
 const updateSlide = async (id, body) => {
-  const slide = await slideRepository.getById({ where: { slideId : id } });
+  try {
+    const slide = await slideRepository.getById(id);
 
-  if (slide) {
-    const newSlide = await slideRepository.update(body);
+    if (slide) {
+      const newSlide = await slideRepository.update(
+        id,
+        body
+      );
 
-    if (!newSlide) {
-      throwError('Bad Request', Forbidden);
+      if (!newSlide) {
+        throwError('Bad Request', Forbidden);
+      }
+
+      return newSlide;
+    } else {
+      throwError('Slide not found', NotFound);
     }
-
-    return newSlide;
-
-  } else {
-    throwError('Record not found', NotFound);
-  };
-}
+  } catch (error) {
+    throwError(error);
+  }
+};
 
 module.exports = { create, getById, updateSlide };
