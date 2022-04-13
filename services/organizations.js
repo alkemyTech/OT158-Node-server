6288957f97d3fffda764e17991c37626aee8b497
Martin L. Ruiz <alacran.mlr@gmail.com>
2022-04-13 11:44:38 -0300

    trycatch agregado

--- services/organizations.js
+++ services/organizations.js
@@ -2,15 +2,20 @@ const { getOne } = require('../repositories/organizations');
 const { NotFound } = require('../utils/status');
 
 const getDataOrganization = async () => {
-  const organization = await getOne();
+  try {
+    const organization = await getOne();
 
-  if (!organization) {
-    const error = new Error('Organization not found');
-    error.status = NotFound;
-    throw error;
+    if (!organization) {
+      const error = new Error('Organization not found');
+      error.status = NotFound;
+      throw error;
+    }
+
+    return organization;
+
+  } catch (error) {
+    error;
   }
-
-  return organization;
 };
 
 module.exports = { getDataOrganization };
