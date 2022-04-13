6e8d68851bb884154bbb09a982d3a5e2907e4ebf
Mario Del Campo <mdcgithubprojects@gmail.com>
2022-04-13 09:46:48 -0400

    Resolviendo conflictos

--- app.js
+++ app.js
@@ -53,16 +53,6 @@ app.use(function(req, res, next) {
 });
 
 // error handler
-<<<<<<< HEAD
-app.use(function(err, req, res, next) {
-	// set locals, only providing error in development
-	res.locals.message = err.message;
-	res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-	// render the error page
-	res.status(err.status || 500);
-	res.render('error');
-=======
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
@@ -72,7 +62,6 @@ app.use(function (err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
->>>>>>> dev
 });
 
 module.exports = app;
