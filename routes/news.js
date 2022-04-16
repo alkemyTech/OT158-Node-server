c2aeaee1ceab54c1767ca6fdfbaa66115c4ba163
giornn0 <shaaamsuu@gmail.com>
2022-04-16 09:33:31 -0300

    fixed improt adminValidator

--- routes/news.js
+++ routes/news.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const newsController = require('../controllers/news');
-const adminValidator = require('../middlewares/adminValidator');
+const { adminValidator } = require('../middlewares/adminValidator');
 const { validData } = require('../middlewares/news');
 
 router.post('/', validData, newsController.create);
