50c7cd409effc57ebb7f612f14d1ea27a41d770b
Matias Minchaca <matiasminchaca1@gmail.com>
2022-04-18 16:51:17 -0300

    adding 'getTokenFromHeaders' function

--- middlewares/ownership.js
+++ middlewares/ownership.js
@@ -1,41 +1,52 @@
 const { verifyToken } = require('../modules/auth');
-const { roleAdmin } = require('../config/config').development;
-const { Unauthorized, Forbidden } = require('../utils/status')
-const { throwError } = require('../utils/errorHandler')
+const { roleAdmin } =
+  require('../config/config').development;
+const {
+  Unauthorized,
+  Forbidden
+} = require('../utils/status');
+const { throwError } = require('../utils/errorHandler');
 
-const ownershipValidator = (req, res, next) => {
-  const authorization = req.get('authorization');
-
-  const token = tokenExtraction(authorization);
-
+const ownershipValidator = async (req, res, next) => {
   const { id } = req.params;
 
-  if (!authorization) throwError('No token provided', Unauthorized);
+  const getTokenFromHeaders = () => {
+    const authorization = req.get('authorization');
+
+    if (!authorization) {
+      throwError('No token provided', Unauthorized);
+    }
+
+    const tokenFormatting = authorization.substring(7);
+
+    return tokenFormatting;
+  };
 
   try {
+    const token = getTokenFromHeaders();
+
     const decoded = verifyToken(token);
 
-    req.userId = decoded.userId.toString();
+    req.userData = {
+      userId: decoded.userId.toString(),
+      roleId: decoded.roleId?.toString()
+    };
 
-    req.roleId = decoded.roleId?.toString();
-
-    if (req.roleId && req.roleId === roleAdmin?.toString()) {
+    if (
+      req.userData.roleId &&
+      req.userData.roleId === roleAdmin?.toString()
+    ) {
       return next();
-    };
+    }
 
-    if (req.userId !== id.toString()) {
+    if (req.userData.userId !== id.toString()) {
       throwError('Invalid token!', Forbidden);
-    };
+    }
 
     next();
-
   } catch (err) {
     next(err);
-  };
-};
-
-const tokenExtraction = (auth) => {
-  return auth?.substring(7)
+  }
 };
 
 module.exports = { ownershipValidator };
