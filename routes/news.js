e6c28ec0f8f50af8a179277b3791c33a652c5460
Edgar-Rios <sistunla2020@gmail.com>
2022-04-19 09:55:32 -0300

    added fields validations to update

--- routes/news.js
+++ routes/news.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router();
 const newsController = require('../controllers/news');
 const { validData } = require('../middlewares/news');
+const { updateForm } = require('../validations/newsValidator');
 
 router.post('/', validData, newsController.create);
 router.get('/', newsController.getAll);
-router.put('/:id', newsController.update);
+router.put('/:id', updateForm, newsController.update);
 
 module.exports = router;
