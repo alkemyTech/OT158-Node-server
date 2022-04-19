35b4f6f3bc6504d925dcb635b155c21a3c639440
Edgar-Rios <sistunla2020@gmail.com>
2022-04-19 09:58:21 -0300

    added admin validator to route update

--- routes/news.js
+++ routes/news.js
@@ -3,9 +3,10 @@ const router = express.Router();
 const newsController = require('../controllers/news');
 const { validData } = require('../middlewares/news');
 const { updateForm } = require('../validations/newsValidator');
+const { adminValidator } = require('../middlewares/adminValidator');
 
 router.post('/', validData, newsController.create);
 router.get('/', newsController.getAll);
-router.put('/:id', updateForm, newsController.update);
+router.put('/:id', adminValidator, updateForm, newsController.update);
 
 module.exports = router;
