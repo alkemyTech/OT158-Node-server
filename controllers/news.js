ed26ad86f3dbf5ab6a6898384bfa285f29517944
giornn0 <shaaamsuu@gmail.com>
2022-04-19 15:12:53 -0300

    fixed response

--- controllers/news.js
+++ controllers/news.js
@@ -1,6 +1,10 @@
 const newsService = require('../services/news');
-const { OK, Created, NotFound } = require('../utils/status');
-const {throwError} = require('../utils/errorHandler');
+const {
+  OK,
+  Created,
+  NotFound
+} = require('../utils/status');
+const { throwError } = require('../utils/errorHandler');
 
 const getAll = async (req, res, next) => {
   try {
@@ -20,17 +24,18 @@ const create = async (req, res, next) => {
   }
 };
 
-const getById = async (req, res,next)=>{
+const getById = async (req, res, next) => {
   try {
-    const {id} = req.params;
+    const { id } = req.params;
     const newFinded = await newsService.getById(id);
-    if (!newFinded)
-      throwError('Inexistent new!',NotFound);
-    return res.status();
+
+    if (!newFinded) throwError('Inexistent new!', NotFound);
+
+    res.status(OK).json({ data: newFinded });
   } catch (error) {
     next(error);
   }
-}
+};
 module.exports = {
   getAll,
   create,
