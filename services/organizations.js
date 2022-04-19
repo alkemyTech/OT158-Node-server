f06f60e1a9b00d4688fd96977438c20f4ccc8697
Edgar-Rios <sistunla2020@gmail.com>
2022-04-19 10:50:19 -0300

    fixes

--- services/organizations.js
+++ services/organizations.js
@@ -1,3 +1,4 @@
+const { validationResult } = require('express-validator');
 const {
   getOne,
   update,
@@ -7,9 +8,6 @@ const {
   NotFound,
   BadRequest,
 } = require('../utils/status');
-const {
-  validationResult
-} = require('express-validator');
 
 const getDataOrganization = async () => {
   try {
@@ -29,30 +27,31 @@ const getDataOrganization = async () => {
 };
 
 const updateOrganization = async (req) => {
+  try {
+    
+    if (!validationResult(req).isEmpty()) {
+      return Promise.reject({
+        status: BadRequest,
+        message: "errores de formulario",
+      });
+    }
 
-  if (!validationResult(req).isEmpty()) {
-    return Promise.reject({
-      status: BadRequest,
-      message: "errores de formulario",
-    });
+    const { id } = req.params;
+    const changes = {...req.body};
+
+    const organization = await getById(id);
+    if (!organization) return Promise.reject({
+      message: "id non exists",
+      status: NotFound,
+    })
+
+    const isUpdated = await update(id, changes);
+
+    return isUpdated? await getById(id) : Promise.reject("unknow problems");
+
+  } catch (error) {
+    Promise.reject("unknow problems");
   }
-
-  const {
-    id
-  } = req.params;
-  const changes = {
-    ...req.body,
-  };
-
-  const organization = await getById(id);
-  if (!organization) return Promise.reject({
-    message: "id non exists",
-    status: NotFound,
-  })
-
-  const isUpdated = await update(id, changes);
-
-  return isUpdated? await getById(id) : Promise.reject("unknow problems")
 }
 
 module.exports = {
