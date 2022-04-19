91fa1cbbea4f87a5aeb23d9274ca8dbcbece6270
JuanSilva27 <juan.silva.0270@gmail.com>
2022-04-19 19:51:39 -0300

    se arregla repositorio, servicio y controller de comments, y se eliminan rutas de prueba

--- routes/comments.js
+++
@@ -1,8 +0,0 @@
-/* const express = require('express');
-const router = express.Router();
-const { getAll } = require("../controllers/comments");
-
-router.get("/",getAll);
-
-module.exports = router;
- */
