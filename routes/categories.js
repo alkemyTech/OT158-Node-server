95b3f59a8abeafea6fd43e829ce9ca2ba77f158a
giornn0 <shaaamsuu@gmail.com>
2022-04-26 07:53:21 -0300

    addedauthenticathed middleware

--- routes/categories.js
+++ routes/categories.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const { create, getAll, update, remove } = require("../controllers/categories");
-const { creationValidation } = require("../middlewares/categories")
+const { creationValidation } = require("../middlewares/categories");
+const {authenticathed} = require("../middlewares/authenticathed");
 const router = express.Router();
 
 /* GET users listing. */
-router.get('/', getAll);
+router.get('/',authenticathed, getAll);
 router.post('/', creationValidation, create);
 router.put("/:id", update)
 router.delete("/:id", remove)
