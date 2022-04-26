3067efe8faa7375847a26c14521c32639857ca4b
JuanSilva27 <juan.silva.0270@gmail.com>
2022-04-26 19:25:00 -0300

    se agrega mail de contacto automatico

--- services/contacts.js
+++ services/contacts.js
@@ -1,7 +1,22 @@
 const contactsRepository = require('../repositories/contacts');
-
+const { throwError } = require('../utils/errorHandler');
+const { BadRequest } = require('../utils/status');
+const { createMessage, sendMail } = require('./mail.service');
 const create = (newContact) => {
-  return contactsRepository.create(newContact);
+  try{
+    const {email, name} = newContact
+    const message = createMessage(
+      email,
+      "Gracias por su consulta",
+      "Gracias por contactarnos",
+      `<h1>${name}, A la brevedad no estaremos comunicando por tu consulta.<h1>`
+    )
+    sendMail(message)
+    return contactsRepository.create(newContact);
+  }
+  catch(error){
+    throwError(BadRequest,"Bad Request")
+  }
 }
 
 
