bad7bf92cdbc4e0250b4c9577edfca06cf2cc099
giornn0 <shaaamsuu@gmail.com>
2022-04-27 09:37:55 -0300

    arranged conditions, and readibility

--- modules/paginator.js
+++ modules/paginator.js
@@ -1,5 +1,5 @@
 const getPageCondition = (page, size)=>{
-  const limit = size ? size : 10;
+  const limit = size || 10;
   const offset = (page-1) * limit;
   return {limit,offset};
 }
@@ -7,9 +7,15 @@ const getPageCondition = (page, size)=>{
 const parsePageResponse = (data, page, limit)=>{
   const { count, rows } = data;
   const totalPages = Math.ceil(count / limit);
-  const previousPage = page != 1 ? `/categories?page=${page-1}` : null;
+  const previousPage = page > 1 ? `/categories?page=${page-1}` : null;
   const nextPage = page < totalPages ? `/categories?page=${page+1}` : null;
-  return {data: rows, previousPage, nextPage, totalPages, itemsPerPage: limit}
+  return {
+    data: rows,
+    previousPage,
+    nextPage,
+    totalPages,
+    itemsPerPage: limit
+  };
 }
 
 module.exports = { getPageCondition, parsePageResponse};
