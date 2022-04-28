92c0c7136041e355b99f500f43b3a7f91b4e1f14
Matias Minchaca <matiasminchaca1@gmail.com>
2022-04-28 17:54:05 -0300

    the route is protected with the admin middleware

--- routes/contacts.js
+++ routes/contacts.js
@@ -2,8 +2,9 @@ const express = require('express');
 const router = express.Router();
 const { contactValidation } = require('../middlewares/contacts');
 const { create, getAll } = require('../controllers/contacts');
+const { adminValidator } = require('../middlewares/adminValidator')
 
-router.get('/', getAll);
+router.get('/', adminValidator, getAll);
 router.post('/', contactValidation, create);
 
 module.exports = router;
