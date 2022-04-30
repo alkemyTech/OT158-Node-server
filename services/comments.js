19a7e8825ff973d8c8cee99f38ba7a664e7980cf
giornn0 <shaaamsuu@gmail.com>
2022-04-30 12:41:42 -0300

    fixed to function condition

--- services/comments.js
+++ services/comments.js
@@ -4,6 +4,11 @@ const newsRepository = require('../repositories/news');
 const { NotFound, Forbidden } = require('../utils/status');
 const { throwError } = require('../utils/errorHandler');
 
+
+const checkOwnershipUser = (user, id)=>{
+  return user.id === id;
+}
+
 const getCommentsByNew = async (req) => {
   const newId = req.params.id
   const condition = {where: {post_id: newId}}
@@ -45,8 +50,8 @@ const removeComment = async (req) => {
     const { id } = req.params;
     const comment = await commentsRepository.getById(id);
   
-    if (comment.user_id === req.user.userId)
-      throwError('Not allowed to perform this action', Forbidden);
+    if(!checkOwnershipUser(req.user, comment.user_id))
+      throwError("User not allowed to perform this action",Forbidden);
   
     return await commentsRepository.remove(id);
   } catch (error) {
