0c85ce2e780846629858fb17e4604f6ce59e340a
Martin L. Ruiz <alacran.mlr@gmail.com>
2022-04-30 20:35:01 -0300

    se agrega seeder para poder probar la paginacion

--- seeders/20220325175850-create-news.js
+++ seeders/20220325175850-create-news.js
@@ -2,15 +2,126 @@
 
 module.exports = {
   async up (queryInterface, Sequelize) {
-    await queryInterface.bulkInsert("News", [{
-      name:"New 1",
-      content:"text example",
-      image:"image.jpg",
-      categoryId:1,
-      type:"novedad",
-      createdAt: new Date,
-      updatedAt: new Date
-    }], {});
+    await queryInterface.bulkInsert("News",
+    [
+      {
+        name:"New 1",
+        content:"text example",
+        image:"image.jpg",
+        categoryId:1,
+        type:"novedad",
+        createdAt: new Date,
+        updatedAt: new Date
+      },
+      {
+        name:"New 2",
+        content:"text example",
+        image:"image.jpg",
+        categoryId:1,
+        type:"novedad",
+        createdAt: new Date,
+        updatedAt: new Date
+      },
+      {
+        name:"New 3",
+        content:"text example",
+        image:"image.jpg",
+        categoryId:1,
+        type:"novedad",
+        createdAt: new Date,
+        updatedAt: new Date
+      },
+      {
+        name:"New 4",
+        content:"text example",
+        image:"image.jpg",
+        categoryId:1,
+        type:"novedad",
+        createdAt: new Date,
+        updatedAt: new Date
+      },
+      {
+        name:"New 5",
+        content:"text example",
+        image:"image.jpg",
+        categoryId:1,
+        type:"novedad",
+        createdAt: new Date,
+        updatedAt: new Date
+      },
+      {
+        name:"New 6",
+        content:"text example",
+        image:"image.jpg",
+        categoryId:1,
+        type:"novedad",
+        createdAt: new Date,
+        updatedAt: new Date
+      },
+      {
+        name:"New 7",
+        content:"text example",
+        image:"image.jpg",
+        categoryId:1,
+        type:"novedad",
+        createdAt: new Date,
+        updatedAt: new Date
+      },
+      {
+        name:"New 8",
+        content:"text example",
+        image:"image.jpg",
+        categoryId:1,
+        type:"novedad",
+        createdAt: new Date,
+        updatedAt: new Date
+      },
+      {
+        name:"New 9",
+        content:"text example",
+        image:"image.jpg",
+        categoryId:1,
+        type:"novedad",
+        createdAt: new Date,
+        updatedAt: new Date
+      },
+      {
+        name:"New 10",
+        content:"text example",
+        image:"image.jpg",
+        categoryId:1,
+        type:"novedad",
+        createdAt: new Date,
+        updatedAt: new Date
+      },
+      {
+        name:"New 11",
+        content:"text example",
+        image:"image.jpg",
+        categoryId:1,
+        type:"novedad",
+        createdAt: new Date,
+        updatedAt: new Date
+      },
+      {
+        name:"New 12",
+        content:"text example",
+        image:"image.jpg",
+        categoryId:1,
+        type:"novedad",
+        createdAt: new Date,
+        updatedAt: new Date
+      },
+      {
+        name:"New 13",
+        content:"text example",
+        image:"image.jpg",
+        categoryId:1,
+        type:"novedad",
+        createdAt: new Date,
+        updatedAt: new Date
+      },
+  ], {});
   },
 
   async down (queryInterface, Sequelize) {
