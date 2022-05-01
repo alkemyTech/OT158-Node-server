541319324f6749c35e0bcf3448a3dcc64b018a0d
JuanSilva27 <juan.silva.0270@gmail.com>
2022-05-01 14:22:26 -0300

    se agrega pagination service para que su implementacion

--- services/pagination.service.js
+++ services/pagination.service.js
@@ -1,5 +1,5 @@
 const getPageCondition = (page, size)=>{
-  const limit = size ? size : 10;
+  const limit = size || 10;
   const offset = page ? (page-1) * limit : 0;
   return {limit,offset};
 }
@@ -8,7 +8,7 @@ const parsePageResponse = (rawData,paginatedData, page, limit, model)=>{
   const count = rawData;
   const totalPages = Math.ceil(count.length / limit);
   if (!page  || page > totalPages) return false;
-  const previousPage = page != 1 ? `/${model}?page=${page - 1}` : null;
+  const previousPage = page > 1 ? `/${model}?page=${page - 1}` : null;
   const nextPage = page < totalPages ? `/${model}?page=${+page + 1}` : null;
   return {
     currentPage:page,
