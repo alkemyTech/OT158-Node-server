4df3552abf44789f60140c230ff9e9344184c399
JuanSilva27 <juan.silva.0270@gmail.com>
2022-05-01 02:38:37 -0300

    se agrega pagination service para que su implementacion

--- services/pagination.service.js
+++ services/pagination.service.js
@@ -1,5 +1,5 @@
 const getPageCondition = (page, size)=>{
-  const limit = size ? size : 5;
+  const limit = size ? size : 10;
   const offset = page ? (page-1) * limit : 0;
   return {limit,offset};
 }
